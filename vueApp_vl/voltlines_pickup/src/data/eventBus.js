86b8311f50af69e46207961d04ace9f66916b5a9
Ali <aligassaloglu@hotmail.co.uk>
2023-05-08 11:24:51 +0300

    update

--- vueApp_vl/voltlines_pickup/src/data/eventBus.js
+++
@@ -1,5 +0,0 @@
-import { ref } from "vue";
-
-export let enrichedPassengerData = []
-export let passangers = [];
-export let routeDetails = {routeDistance: null, routeDuration: null, avgTripDuration: null};
--- vueApp_vl/voltlines_pickup/src/pages/PageNotFound.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="">
-        <h1>Page Not Found</h1>
-    </div>
-</template>
